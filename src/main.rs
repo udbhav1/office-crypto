e1b7efc9aa5e37d13ba7a556c2b18aaa5c889b02
udbhav1 <umuthakana@gmail.com>
2023-01-09 23:49:49 -0500

    impl OleStream and open_helper

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ const DIFSECT: u32 = 0xFFFFFFFC;
 const FATSECT: u32 = 0xFFFFFFFD;
 const ENDOFCHAIN: u32 = 0xFFFFFFFE;
 const FREESECT: u32 = 0xFFFFFFFF;
+const UNKNOWN_SIZE: u32 = 0x7FFFFFFF;
 
 // little-endian
 #[derive(PackedStruct)]
@@ -64,6 +65,7 @@ pub struct OleFile {
     used_streams_fat: Vec<u32>,
     used_streams_minifat: Vec<u32>,
     fat: Vec<u32>,
+    directory_fp: OleStream,
 }
 
 impl OleFile {
@@ -87,6 +89,7 @@ impl OleFile {
             used_streams_fat: Vec::new(),
             used_streams_minifat: Vec::new(),
             fat: Vec::new(),
+            directory_fp: OleStream::default(),
         }
     }
 
@@ -100,6 +103,7 @@ impl OleFile {
         }
 
         self.load_fat();
+        self.load_directory(self.header.first_dir_sector);
     }
 
     fn check_duplicate_stream(&mut self, first_sect: u32, minifat: bool) {
@@ -140,9 +144,31 @@ impl OleFile {
             // TODO finish
             assert!(1 == 0);
         }
+
+        if self.fat.len() as u32 > self.nb_sect {
+            self.fat = self.fat[..self.nb_sect as usize].to_vec();
+        }
+    }
+
+    fn load_directory(&mut self, sect: u32) {
+        self.directory_fp = self.open_helper(sect, UNKNOWN_SIZE, true);
+        let max_entries = self.directory_fp.size / 128;
+        // build direntries and figure out what struct type each is
+    }
+
+    fn open_helper(&mut self, start: u32, size: u32, force_fat: bool) -> OleStream {
+        if size < self.header.mini_stream_cutoff_size && !force_fat {
+            // TODO finish
+            panic!("unreachable");
+        } else {
+            let mut olestream = OleStream::new(start, size, self.sector_size, self.sector_size);
+            olestream.init(&self.raw, &self.fat);
+            olestream
+        }
     }
 
     fn print(&self) {
+        println!("---------------------");
         println!("magic: {:?}", self.header.magic);
         println!("clsid: {:?}", self.header.clsid);
         println!("minor_version: {:?}", self.header.minor_version);
@@ -180,6 +206,73 @@ impl OleFile {
         println!("nb_sect: {:?}", self.nb_sect);
         println!("used_streams_fat: {:?}", self.used_streams_fat);
         println!("used_streams_minifat: {:?}", self.used_streams_minifat);
+        println!("---------------------");
+    }
+}
+
+#[derive(Default, Debug)]
+struct OleStream {
+    start: u32,
+    size: u32,
+    offset: u32,
+    sector_size: u32,
+    stream: Vec<u8>,
+}
+
+impl OleStream {
+    pub fn new(start: u32, size: u32, offset: u32, sector_size: u32) -> Self {
+        OleStream {
+            start,
+            size,
+            offset,
+            sector_size,
+            stream: Vec::new(),
+        }
+    }
+
+    fn init(&mut self, raw: &Vec<u8>, fat: &Vec<u32>) {
+        let mut unknown_size = false;
+        let fat_len = fat.len() as u32;
+
+        if self.size == UNKNOWN_SIZE {
+            self.size = fat_len * self.sector_size;
+            unknown_size = true;
+        }
+
+        let nb_sectors = (self.size + (self.sector_size - 1)) / self.sector_size;
+        assert!(nb_sectors <= fat_len);
+        let mut sect = self.start;
+        assert!(self.size != 0 || sect == ENDOFCHAIN);
+
+        let mut data: Vec<u8> = Vec::new();
+        for _ in 0..nb_sectors {
+            if sect == ENDOFCHAIN {
+                assert!(unknown_size);
+                break;
+            }
+            assert!(sect < fat_len);
+            let start = (self.offset + self.sector_size * sect) as usize;
+            let sector_data = &raw[start..(start + self.sector_size as usize)];
+            // TODO last sector might have less than 512/4k, so read less on index
+            assert!(sector_data.len() == self.sector_size as usize || sect == (fat_len - 1));
+            data.extend_from_slice(&sector_data);
+
+            sect = fat[sect as usize] & 0xFFFFFFFF;
+        }
+
+        if data.len() >= self.size as usize {
+            data = data[..self.size as usize].to_vec();
+        } else if unknown_size {
+            self.size = data.len() as u32;
+        } else {
+            panic!("read less than expected");
+        }
+        // println!(
+        //     "data entries: {:?} {:?} {:?} {:?} {:?} {:?}",
+        //     data[0], data[1], data[2], data[3], data[4], data[5]
+        // );
+        // println!("data len: {:?}", data.len());
+        self.stream = data;
     }
 }
 
@@ -206,12 +299,6 @@ fn sect_to_array(sect: &[u8]) -> &[u32] {
     cast_slice(sect)
 }
 
-// fn open_stream_help(file: &mut OleFile, sect: u32, force_fat: bool) -> OleStream {}
-
-// fn load_directory(file: &mut OleFile, sect: u32) {
-//     let directory_fp = open_stream_help(file, sect, true);
-// }
-
 fn main() {
     let filename = "testFile.docx";
     // let filename = "../../roml/22_8_august.docx";
@@ -224,7 +311,6 @@ fn main() {
 
     let mut olefile = OleFile::new(raw);
     olefile.init();
-
     olefile.print();
 
     // impl _parseinfo and _parseagile to get crypto values and type
